3ae0605b85c624891b37df555ccd90771eb39b24
GroundhogPaul <2265546016@qq.com>
2025-12-04 14:07:21 +0800

    Create learn_rate.py

---
+++ engine/learn_rate.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lr_official(epoch):
+    assert epoch >= 0
+    assert epoch < 8000
+    lrMax = 1e-3
+    lrMin = 1e-5
+    if epoch < 4000:
+        epoch = epoch % (100)
+        if epoch < 50:
+            lr = lrMax - (lrMax - lrMin) * (epoch / 50)
+        else:
+            lr = lrMin + (lrMax - lrMin) * ((epoch - 50) / 50)
+    elif epoch < 8000:
+        epoch = epoch - 4000
+        lr = lrMax - (lrMax - lrMin) / 4000 * epoch 
+    return lr
+
+lstEpochs = list(range(0, 8000, 1))
+lstLrs = [lr_official(epoch) for epoch in lstEpochs]
+plt.plot(lstEpochs, lstLrs)
+plt.xlabel('Epoch')
+plt.ylabel('Learning Rate')
+plt.title('Official Learning Rate Schedule')
+plt.grid()
+plt.show()
+
