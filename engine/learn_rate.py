c1dee10839bca0815b33e47efee4e44fe17327b2
GroundhogPaul <2265546016@qq.com>
2026-02-02 13:48:43 +0800

    no longer use magic number in calculating triangle learning rate

--- engine/learn_rate.py
+++ engine/learn_rate.py
@@ -1,25 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def lr_official(epoch):
+def lr_official(epoch, epochMax):
     assert epoch >= 0
-    assert epoch < 8000
+    assert epoch < epochMax
+
+    epochMaxHalf = epochMax // 2
+    Ntriangle = 40
+    Ttriangle = epochMaxHalf // Ntriangle
+    TtriangleHalf = Ttriangle / 2
     lrMax = 1e-3
     lrMin = 1e-5
-    if epoch < 4000:
-        epoch = epoch % (100)
-        if epoch < 50:
-            lr = lrMax - (lrMax - lrMin) * (epoch / 50)
+    if epoch < epochMaxHalf:
+
+        epoch = epoch % Ttriangle
+        if epoch < TtriangleHalf:
+            lr = lrMax - (lrMax - lrMin) * (epoch /TtriangleHalf)
         else:
-            lr = lrMin + (lrMax - lrMin) * ((epoch - 50) / 50)
-    elif epoch < 8000:
-        epoch = epoch - 4000
-        lr = lrMax - (lrMax - lrMin) / 4000 * epoch 
+            lr = lrMin + (lrMax - lrMin) * ((epoch - TtriangleHalf) / TtriangleHalf)
+    elif epoch < epochMax:
+        epoch = epoch - epochMaxHalf
+        lr = lrMax - (lrMax - lrMin) / epochMaxHalf * epoch 
     return lr
 
 if __name__ == "__main__":
-    lstEpochs = list(range(0, 8000, 1))
-    lstLrs = [lr_official(epoch) for epoch in lstEpochs]
+    epochMax = 8000
+    lstEpochs = list(range(0, epochMax, 1))
+    lstLrs = [lr_official(epoch, epochMax=epochMax) for epoch in lstEpochs]
     plt.plot(lstEpochs, lstLrs)
     plt.xlabel('Epoch')
     plt.ylabel('Learning Rate')
