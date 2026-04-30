06458dea6ad2028f3eafa03628d145d069c2d175
GroundhogPaul <2265546016@qq.com>
2026-04-30 14:15:49 +0800

    Dataset TimBrooks V2 support jpg now

--- data/RawDataset.py
+++ data/RawDataset.py
@@ -3,127 +3,14 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
-
 import utilData
-import imageio
 
-import glob
-import numpy as np
-import torch
-from RawContainer.utilRaw import RawUtils 
-from RawContainer.utilVrf import vrf, CFAPatternEnum, read_vrf, save_vrf_image, save_raw_image 
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
+from torch.utils.data import Dataset
 
-import matplotlib.pyplot as plt
-from matplotlib.ticker import ScalarFormatter
-from utils.KSigma import KSigma, Official_Ksigma_params
-from utils.NLM import NLM_rggb_withPad
-import torchvision.transforms as tvtransforms
-import copy
-import cv2
-import math
-import time
-from utilBasic import print_gpu_memory_stats
-
-from ImgDataset import CropDatasetVrf, CropDatasetJpg
-from AddNoise import add_noise_Concat, add_noise_KSigma
-
-class NumpyHW2TorchChHW:
-    """[H,W] ndarray ---> [1,H,W] Tensor"""
-    def __call__(self, arr):
-        return torch.from_numpy(arr).float().unsqueeze(0)  # [1, H, W]
-
-class RawDatasetTimBrooks(Dataset):
+class RawDatasetBasic(Dataset):
     def __init__(self, lstImgPattern, Hbayer, Wbayer, device, CropDataset):
         self.CropDataset = CropDataset(lstImgPattern, Hbayer, Wbayer, device = device)
         self.device = device
 
-    def __getitem__(self, idx):
-        HWCh_rggb_01, mete_data = self.CropDataset[idx]
-        ChHW_rggb_01 = HWCh_rggb_01.permute(2, 0, 1)
-        noisy, noise_var = add_noise_Concat(ChHW_rggb_01)
-        gt = ChHW_rggb_01
-        return gt, noisy, noise_var, mete_data
-    
     def __len__(self):
         return len(self.CropDataset)
-
-    def ChHW4n_2_bgr888(self, ChHW4n, meta_data, bCPU=True):
-        assert isinstance(meta_data, dict)
-        HWCh4n = ChHW4n.permute(1, 2, 0)
-        bgr888 = self.CropDataset.HWCh4n_2_bgr888(HWCh4n, meta_data, bCPU = bCPU)
-        return bgr888
-
-def my_collate(batch):
-    imgs = torch.stack([b[0] for b in batch])
-    noisys = torch.stack([b[1] for b in batch])
-    vars = torch.stack([b[2] for b in batch])
-    metas = [b[3] for b in batch]
-    return imgs, noisys, vars, metas
-
-def create_dataloader(dataset, batch_size, num_workers=0):
-    """Creates a DataLoader for unprocessing training.
-    
-    Args:
-        dir_pattern: A string representing source data directory glob.
-        height: Height to crop images.
-        width: Width to crop images.
-        batch_size: Number of training examples per batch.
-        num_workers: Number of workers for parallel data loading.
-        
-    Returns:
-        A PyTorch DataLoader instance.
-    """
-    prefetch_factor = None
-    # prefetch_factor = num_workers if num_workers > 0 else None
-    # prefetch_factor = num_workers * 2 if num_workers > 0 else None
-    return DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        drop_last=True,  # To ensure consistent batch sizes
-
-        num_workers = num_workers,
-        prefetch_factor = prefetch_factor,
-        persistent_workers = num_workers > 0,
-
-        # pin_memory=True,
-        # pin_memory_device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu'),
-        
-        # timeout=100000,
-        collate_fn = my_collate,
-    )
-
-if __name__ == "__main__":
-    seed = 39
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-
-    device = 'cuda:2'
-    device = torch.device(device if torch.cuda.is_available() else 'cpu')
-
-    # ----- test vrf ----- #
-    # Hbayer, Wbayer = 1024, 1024
-    # dir_pattern = "D:/image_database/SID/SID/Sony/longVRFmini/*.vrf"
-    # dataset = RawDatasetBasic(dir_pattern, Hbayer, Wbayer, device=device, CropDataset=CropDatasetVrf)
-    # train_loader = create_dataloader(dataset, 1, num_workers=0)
-
-    # ----- test jpg ----- #
-    Hbayer, Wbayer = 512, 512
-    dir_pattern = "D:/image_database/mirflickr25k/mirflickr/*.jpg"
-    dataset = RawDatasetTimBrooks(dir_pattern, Hbayer, Wbayer, device=device, CropDataset=CropDatasetJpg)
-    train_loader = create_dataloader(dataset, 4, num_workers=0)
-
-    for batch_idx, (BChHW_gt, BChHW_noisy, BChHW_var, meta_datas) in enumerate(train_loader):
-
-        bgr888_gt = dataset.ChHW4n_2_bgr888(BChHW_gt[0], meta_datas[0], True)
-        cv2.imwrite("RawDatasetTimBrooks_gt.jpg", bgr888_gt)
-
-        bgr888_noisy = dataset.ChHW4n_2_bgr888(BChHW_noisy[0], meta_datas[0], True)
-        cv2.imwrite("RawDatasetTimBrooks_noisy.jpg", bgr888_noisy)
-
-        bgr888_var = dataset.ChHW4n_2_bgr888(BChHW_var[0], meta_datas[0], True)
-        cv2.imwrite("RawDatasetTimBrooks_var.jpg", bgr888_var)
-
-        break
--- data/RawDatasetTimBrooks.py
+++ data/RawDatasetTimBrooks.py
@@ -2,142 +2,120 @@ import sys
 import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
-    sys.path.insert(0, current_dir)  # 插入开头，优先搜索
+    sys.path.insert(0, current_dir)
 
 import utilData
-import utilBasic
 import imageio
+
 import glob
 import numpy as np
 import torch
-from utilRaw import RawUtils
-from utilArw import ArwReader
+from RawContainer.utilRaw import RawUtils 
+from RawContainer.utilVrf import vrf, CFAPatternEnum, read_vrf, save_vrf_image, save_raw_image 
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
+from utils.KSigma import KSigma, Official_Ksigma_params
+from utils.NLM import NLM_rggb_withPad
 import torchvision.transforms as tvtransforms
 import copy
 import cv2
 import math
+import time
 from utilBasic import print_gpu_memory_stats
 
-from RawDataset import RawArrayToTensor, PMRIDRawDataset, create_dataloader 
+from ImgDataset import CropDatasetVrf, CropDatasetJpg
+from RawDataset import RawDatasetBasic
+from AddNoise import add_noise_Concat
 
-class TimBrooksRawDataset(PMRIDRawDataset):
-    def __init__(self, dir_pattern, height=1024, width=1024, bPreLoadAll=False, device='cpu'):
-        super().__init__(dir_pattern, height, width, bPreLoadAll=bPreLoadAll, device=device)
+class RawDatasetTimBrooks(RawDatasetBasic):
+    def __init__(self, lstImgPattern, Hbayer, Wbayer, device, CropDataset):
+        super().__init__(lstImgPattern, Hbayer, Wbayer, device=device, CropDataset=CropDataset)
 
-    def add_noise(self, input_bayer_01, noise_type='Gaussian'):
-        log_min_shot_noise = torch.log(torch.tensor(0.0001))
-        log_max_shot_noise = torch.log(torch.tensor(0.012))
-        log_shot_noise =  log_min_shot_noise + (log_max_shot_noise - log_min_shot_noise) * torch.rand(1)
-        shot_noise = torch.exp(log_shot_noise).item()
+    def __getitem__(self, idx):
+        HWCh_rggb_01, mete_data = self.CropDataset[idx]
+        ChHW_rggb_01 = HWCh_rggb_01.permute(2, 0, 1)
+        noisy, noise_var = add_noise_Concat(ChHW_rggb_01)
+        gt = ChHW_rggb_01
+        return gt, noisy, noise_var, mete_data
 
-        line = lambda x: 2.18 * x + 1.2
-        log_read_noise = line(log_shot_noise) + torch.normal(mean = 0.0, std = 0.26, size=())
-        read_noise = torch.exp(log_read_noise).item()
+    def ChHW4n_2_bgr888(self, ChHW4n, meta_data, bCPU=True):
+        assert isinstance(meta_data, dict)
+        HWCh4n = ChHW4n.permute(1, 2, 0)
+        bgr888 = self.CropDataset.HWCh4n_2_bgr888(HWCh4n, meta_data, bCPU = bCPU)
+        return bgr888
 
-        variance = input_bayer_01 * shot_noise + read_noise
-        noise = torch.randn_like(input_bayer_01) * torch.sqrt(variance)
+def my_collate(batch):
+    imgs = torch.stack([b[0] for b in batch])
+    noisys = torch.stack([b[1] for b in batch])
+    vars = torch.stack([b[2] for b in batch])
+    metas = [b[3] for b in batch]
+    return imgs, noisys, vars, metas
+
+def create_dataloader(dataset, batch_size, num_workers=0):
+    """Creates a DataLoader for unprocessing training.
+    
+    Args:
+        dir_pattern: A string representing source data directory glob.
+        height: Height to crop images.
+        width: Width to crop images.
+        batch_size: Number of training examples per batch.
+        num_workers: Number of workers for parallel data loading.
         
-        noisy_bayer01 = input_bayer_01 + noise
-        return torch.clamp(noisy_bayer01.to(torch.float32), 0, 1), copy.deepcopy(shot_noise), copy.deepcopy(read_noise) 
-    
-    def __getitem__(self, index):
-        # read raw
-        if self.bPreLoadAll:
-            ArwReaderCur = self.preloaded_raw[index]
-            input_bayer = self.preloaded_bayer_device[index] # GPU buffer
-        else:
-            ArwReaderCur = ArwReader(self.filenames[index])
-            # ArwReaderCur = ArwReader("D:/image_database/SID/SID/Sony/long/00002_00_10s.ARW") # test code
-            input_bayer = ArwReaderCur.raw_image.astype(np.float32)
-            input_bayer = np.ascontiguousarray(input_bayer)
-            input_bayer = RawArrayToTensor()(input_bayer)
-            input_bayer = input_bayer.to(self.device)
-        bayer_pattern = ArwReaderCur.raw_pattern
-        white_level = ArwReaderCur.white_level
-        black_level = ArwReaderCur.black_level_per_channel[0]
-        B, H, W = input_bayer.shape
+    Returns:
+        A PyTorch DataLoader instance.
+    """
+    prefetch_factor = None
+    # prefetch_factor = num_workers if num_workers > 0 else None
+    # prefetch_factor = num_workers * 2 if num_workers > 0 else None
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,  # To ensure consistent batch sizes
 
-        # data transform          
-        input_bayer = self.random_crop_and_flip(input_bayer, bayer_pattern, H_crop=self.height, W_crop=self.width, p_flip_ud=0.5, p_flip_lr=0.5)
-        # input_bayer_01 = input_bayer / white_level  # no black_level at all, copied from benchmark.py.BenchmarkLoader
-        # input_bayer_01 = (input_bayer - black_level) / (white_level - black_level)
-        input_bayer_01 = (input_bayer - black_level) / white_level
+        num_workers = num_workers,
+        prefetch_factor = prefetch_factor,
+        persistent_workers = num_workers > 0,
 
-        # brightness and contrast augmentation
-        input_bayer_01 = tvtransforms.ColorJitter(brightness=(0.2, 1.2), contrast=(0.5, 1.5))(input_bayer_01)
-        input_bayer_01 = torch.clamp(input_bayer_01, 0.0, 1.0)
-        input_rggb_01 = RawUtils.bayer_to_rggb(input_bayer_01, "RGGB")  # to [H/2, W/2, 4] RGGB
-
-        # add random noise
-        input_rggb_01_noisy, shot_noise, read_noise = self.add_noise(input_rggb_01)
-        # print("\n train set: shot_noise:", shot_noise.item(), " read_noise:", read_noise.item())
-        input_rggb_variance = torch.sqrt(input_rggb_01_noisy * shot_noise + read_noise)
-
-        # save meta data
-        wb_gain = ArwReaderCur.GetWBgain01("RGGB")
-        ccm3x3 = ArwReaderCur.GetCCM()
-        meta_data = {
-            'black_level': black_level,
-            'wb_gain': wb_gain,
-            'ccm': ccm3x3
-        }
-
-        input_rggb_01 = input_rggb_01.permute(2,0,1).to(self.device)
-        input_rggb_01_noisy = input_rggb_01_noisy.permute(2,0,1).to(self.device)
-        input_rggb_variance = input_rggb_variance.permute(2,0,1).to(self.device)
-        return input_rggb_01, input_rggb_01_noisy, input_rggb_variance, meta_data
-
-    @classmethod
-    def ConvertDatasetImgToBGR888(cls, inputs_rggb, meta_data, idx = 0):
-
-        input_rggb = inputs_rggb[idx]
-        input_rggb = input_rggb.permute(1, 2, 0).to('cpu')
-
-        if hasattr(input_rggb, 'permute'):    # Pytorch
-            input_rggb = input_rggb.detach().cpu().numpy()
-        elif hasattr(input_rggb, 'shape'):      # Numpy
-            pass
-        else:
-            raise NotImplementedError("Input must be a numpy array or pytorch tensor, current type: {}".format(type(input_rggb)))
-
-        wb_gain = meta_data["wb_gain"][idx]
-        if hasattr(wb_gain, 'permute'):    # Pytorch
-            wb_gain = wb_gain.detach().numpy()
-        CCM = meta_data["ccm"][idx]
-        if hasattr(CCM, 'permute'):    # Pytorch
-            CCM = CCM.detach().numpy()
-    
-        input_rgb = RawUtils.bayer01_2_rgb01(
-            RawUtils.rggb2bayer(input_rggb), wb_gain=wb_gain, CCM=CCM, gamma = 2.2)
-        input_bgr = cv2.cvtColor(input_rgb, cv2.COLOR_RGB2BGR)
-        input_bgr = (input_bgr*255.0).astype(np.uint8)
-
-        return input_bgr
+        # pin_memory=True,
+        # pin_memory_device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu'),
+        
+        # timeout=100000,
+        collate_fn = my_collate,
+    )
 
 if __name__ == "__main__":
-    dir_pattern = "D:/image_database/SID/SID/Sony/long/*.ARW"
-    device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
-    # device = 'cpu'
-    bPreLoadAll = False
-    np.random.seed(42)
+    seed = 39
+    torch.manual_seed(seed)
+    np.random.seed(seed)
 
-    dataset = TimBrooksRawDataset(dir_pattern, 1024, 1024, bPreLoadAll=False, device=device)
-    train_loader = create_dataloader(dataset, 2)
+    device = 'cuda:2'
+    device = torch.device(device if torch.cuda.is_available() else 'cpu')
 
-    for batch_idx, (inputs_rggb_gt, inputs_rggb_noisy, inputs_rggb_variance, meta_data) in enumerate(train_loader):
+    # ----- test vrf ----- #
+    # Hbayer, Wbayer = 1024, 1024
+    # dir_pattern = "D:/image_database/SID/SID/Sony/longVRFmini/*.vrf"
+    # dataset = RawDatasetBasic(dir_pattern, Hbayer, Wbayer, device=device, CropDataset=CropDatasetVrf)
+    # train_loader = create_dataloader(dataset, 1, num_workers=0)
 
-        bgr888_noisy = dataset.ConvertDatasetImgToBGR888(inputs_rggb_noisy, meta_data, 0)
-        cv2.imwrite("rgb_noisy.bmp", bgr888_noisy)
+    # ----- test jpg ----- #
+    Hbayer, Wbayer = 512, 512
+    dir_pattern = "D:/image_database/mirflickr25k/mirflickr/*.jpg"
+    dataset = RawDatasetTimBrooks(dir_pattern, Hbayer, Wbayer, device=device, CropDataset=CropDatasetJpg)
+    train_loader = create_dataloader(dataset, 4, num_workers=0)
 
-        bgr888_gt = dataset.ConvertDatasetImgToBGR888(inputs_rggb_gt, meta_data)
-        cv2.imwrite("rgb_gt.bmp", bgr888_gt)
+    for batch_idx, (BChHW_gt, BChHW_noisy, BChHW_var, meta_datas) in enumerate(train_loader):
+
+        bgr888_gt = dataset.ChHW4n_2_bgr888(BChHW_gt[0], meta_datas[0], True)
+        cv2.imwrite("RawDatasetTimBrooks_gt.jpg", bgr888_gt)
+
+        bgr888_noisy = dataset.ChHW4n_2_bgr888(BChHW_noisy[0], meta_datas[0], True)
+        cv2.imwrite("RawDatasetTimBrooks_noisy.jpg", bgr888_noisy)
+
+        bgr888_var = dataset.ChHW4n_2_bgr888(BChHW_var[0], meta_datas[0], True)
+        cv2.imwrite("RawDatasetTimBrooks_var.jpg", bgr888_var)
 
         break
-
-    filename = os.path.basename(__file__) if '__file__' in globals() else os.path.basename(sys.argv[0])
-    print(filename, ": Image saved as output.jpg")
