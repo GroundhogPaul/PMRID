9733886bac8c37c1ab3b198cd2b5aafb5991235d
GroundhogPaul <2265546016@qq.com>
2026-05-18 15:07:41 +0800

    Create utilStudy.py

---
+++ 02_Study/utilStudy.py
@@ -0,0 +1,13 @@
+import cv2
+import sys
+from pathlib import Path
+import os
+import numpy as np
+
+# ----- add the parent folder to environment ----- 
+ROOT = Path(__file__).resolve().parents[1]
+ROOT = Path(__file__).parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+
+import utilBasic
