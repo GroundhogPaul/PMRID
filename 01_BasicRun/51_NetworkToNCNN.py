26b7e8ab01dcc3aba88e0912b504823acc1f915e
GroundhogPaul <2265546016@qq.com>
2026-06-29 15:25:16 +0800

    Update 51_NetworkToNCNN.py

--- 01_BasicRun/51_NetworkToNCNN.py
+++ 01_BasicRun/51_NetworkToNCNN.py
@@ -6,47 +6,42 @@ import os
 import re
 import cv2
 import pnnx
-from utilRaw import RawUtils
-from utils.KSigma import KSigma, Official_Ksigma_params 
-from utilVrf import vrf, read_vrf, save_vrf_image, save_raw_image, CFAPatternEnum, FlipBayerPattern2Pattern
+# from utils.KSigma import KSigma, Official_Ksigma_params 
+# from utilVrf import vrf, read_vrf, save_vrf_image, save_raw_image, CFAPatternEnum, FlipBayerPattern2Pattern
 # from models.net_torch import NetworkPMRID as Network
-from models.net_torch_SCH import Network_Level3_ch_off_bilinear as Network
+# from models.net_torch_SCH import Network_Level3_ch_off_bilinear as Network
+# from models.net_torch_noahtcv import NOAHTCV as Network
+from models.net_torch_noah_add import NOAH_L2_add as Network
 import torch
 
 if __name__ == '__main__':
-    Wout, Hout = 4224, 3136
+    Wbayer, Hbayer = 1024, 1024
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # ---------- read model ---------- #
     # ----- assert ckpt paths ----- #
-    model_folder, inp_scale =  "./runs/models/Huan2.4G_fiveK_subset230_default_color/top_models/", 256
-    model_name = "latest_modelK_psnr0.00_e7803s218500_lr5.00e-04.pth"
-
+    model_folder =  "./runs/models/NOAH_L2_add/DumpCkpt"
+    model_name = "008200_L0.0059.ckpt"
     model_path = os.path.join(model_folder, model_name)
-    assert os.path.exists(model_path), f"Model file does not exist: {model_path}"
-    match = re.search(r'e(?P<epoch>\d+)s(?P<step>\d+)', model_name)
-    epoch = int(match.group('epoch'))
-    step = int(match.group('step'))
+    # assert os.path.exists(model_path), f"Model file does not exist: {model_path}"
 
     # ----- get model name -----
     path_parts = model_path.split('/')
     models_index = path_parts.index('models')
-    # sImgSuffix = path_parts[models_index + 1] + f"_netK{epoch}"
-    sImgSuffix = "HuanKSigma"
-    print("sImgSuffix = ", sImgSuffix)
-    assert os.path.exists(model_path), f"Model file does not exist: {model_path}"
+    model_name = path_parts[models_index + 1]
+    print(model_name)
 
     # ----- output folder ----- #
     sModel_folder = os.path.dirname(os.path.dirname(model_path))
 
     # ----- load ckpt ----- #
-    net = Network(mode="KSigma").to(device)
-    net.load_CKPT(str(model_path), device=torch.device(device))
+    net = Network().to(device)
+    # net.load_CKPT(str(model_path), device=torch.device(device))
     net.eval()
 
-    dummy_input = torch.randn(1, 4, Hout//2, Wout//2).to(device)
+    dummy_input = torch.randn(1, 8, Hbayer//2, Wbayer//2).to(device)
 
     print(dummy_input.shape)
     print(dummy_input.dtype)
 
-    opt_model = pnnx.export(net, sImgSuffix, dummy_input)
+    opt_model = pnnx.export(net, model_name, dummy_input)
